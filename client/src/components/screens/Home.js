d7e3ad10b8a3b88967fbd83df5f75e3a02bf1dcf
mansmahamat <mansour.mahamat.salle@gmail.com>
2022-01-10 08:06:29 +0100

    :art: add frontend
    add frontend

---
+++ client/src/components/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='bg-red-100'>
+            Hellos
+            sdsf
+            ffd
+        </div>
+    )
+}
+
+export default Home
---
+++ client/src/components/screens/PrivateScreen.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const PrivateScreen = () => {
+  const [error, setError] = useState('');
+  const [privateData, setPrivateData] = useState('');
+
+  useEffect(() => {
+    const fetchPrivateDate = async () => {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('authToken')}`
+        }
+      };
+
+      try {
+        const { data } = await axios.get('/api/private', config);
+        setPrivateData(data.data || 'Helloooo');
+      } catch (error) {
+        localStorage.removeItem('authToken');
+        setError('You are not authorized please login');
+      }
+    };
+
+    fetchPrivateDate();
+  }, []);
+  return error ? (
+    <span className="error-message">
+      {error} <Link to="/login">CLICK</Link>
+    </span>
+  ) : (
+    <div>{privateData} </div>
+  );
+};
+
+export default PrivateScreen;
