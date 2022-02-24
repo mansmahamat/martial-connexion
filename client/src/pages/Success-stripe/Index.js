07445e9b52b5d2a7fb32293290589d22b3965544
mansmahamat <mansour.mahamat.salle@gmail.com>
2022-02-24 21:12:07 +0100

    :art: finish
    finish

---
+++ client/src/pages/Success-stripe/Index.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+// cs_test_a1dXao8fw6yu4KAXprsEtvMe911duPcBUnlikLOQhvAlQrYbXuX9iqVGWn
+
+export const SuccessDisplay = ({ sessionId }) => {
+  return (
+    <section>
+      <div className="product Box-root">
+        LOGO {sessionId}
+        <div className="description Box-root">
+          <h3>Subscription to starter plan successful!</h3>
+        </div>
+      </div>
+      <form action="/create-portal-session" method="POST">
+        <input type="hidden" id="session-id" name="session_id" value={sessionId} />
+        <button id="checkout-and-portal-button" type="submit">
+          Manage your billing information
+        </button>
+      </form>
+    </section>
+  );
+};
