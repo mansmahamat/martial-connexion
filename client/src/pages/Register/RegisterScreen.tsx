9f2d4c8ae3f551d3bf61e948d8dfb439f5fa04f4
mansmahamat <mansour.mahamat.salle@gmail.com>
2022-06-06 09:19:03 +0200

    fix

--- client/src/pages/Register/RegisterScreen.tsx
+++ client/src/pages/Register/RegisterScreen.tsx
@@ -310,17 +310,6 @@ const RegisterScreen = () => {
       </form>
     </div>
 
-    // <div className="min-h-screen flex">
-    //   <div className="flex-1 overflow-hidden flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-    //     <div className="mx-auto w-full max-w-sm lg:w-96">
-    //       <div>
-    //         <h2 className="mt-6 text-3xl text-center font-extrabold ">Register</h2>
-    //       </div>
-
-    //       <div className="mt-8">
-    //         <div className="mt-6">
-    //           <form
-    //             onSubmit={(e) => {
     //               e.preventDefault();
     //               handleSubmit();
     //             }}
